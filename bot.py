1289aed6ecc79852f2b3b1a581824d96ec87d159
Jacob1010-h <lordgaming1010@gmail.com>
2022-11-15 17:48:35 -0800

    Create print message method
    added a print method message to print things to the console with a timestamp and more information

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot.py
 import os
 import tbapy
-
+from datetime import datetime
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -13,29 +13,25 @@ TBA_TOKEN = os.getenv('TBA_TOKEN')
 tba = tbapy.TBA(TBA_TOKEN)
 
 intents = discord.Intents.all()
-# intents.messages = True
-# intents.members = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
-#
+
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print_message('success', 'discord.client', 'Bot is connected to Discord')
 
-# @client.command()
-# async def ping(ctx):
-#     await ctx.send('pong')
+
+'''
+It deletes the command message and then deletes the amount of messages specified by the user
+
+@param ctx: The context of where the command was used
+@param amount: The amount of messages to delete, defaults to 5 (optional)
+'''
 
 
 @client.command()
 async def clear(ctx, amount=5):
-    """
-    It deletes the command message and then deletes the amount of messages specified by the user
-
-    :param ctx: The context of where the command was used
-    :param amount: The amount of messages to delete, defaults to 5 (optional)
-    """
     if amount == -00:
         await ctx.channel.purge()
     else:
@@ -47,70 +43,138 @@ async def clear(ctx, amount=5):
 async def last(ctx, team_number, year):
     team_number = int(team_number)
     year = int(year)
-    # send a list of the last 5 events from team 4738
+
     events = tba.team_events(team_number, year)
+
     message = ''
     if len(events) == 0:
+        print_message('error', 'tba.team_events', f'No events found for team {team_number} in year {year}')
+
         await ctx.send(f'Team {team_number} did not compete in {year}')
+
     else:
         message += f'Team {team_number} competed in {year} at:\n'
+
         for event in events:
             message += f'{event.name}\n'
+
+        print_message('info', 'tba.team_events', f'Successfully sent {team_number} events in year {year} to channel')
+
         await ctx.send(message)
+
 
 @client.command()
 async def awards(ctx, team_number, year):
     team_number = int(team_number)
     year = int(year)
-    #send a list of the awards won by team_number in year
+
     awards = tba.team_awards(team_number, year)
+
     message = ""
     if len(awards) == 0:
+        print_message('error', 'tba.team_awards', f'No awards found for team {team_number} in year {year}')
+
         await ctx.send(f'Team {team_number} did not win any awards in {year}')
+
     else:
         message += (f'Team {team_number} won the following awards in {year}:\n')
+
         for award in awards:
             message += f"{award['name']}\n"
+
+        print_message('info', 'tba.team_awards', f'Successfully sent {team_number} awards in year {year} to channel')
+
         await ctx.send(message)
 
-#get a robot's stats using tba.team_robots(team_number)
+
+# get a robot's stats using tba.team_robots(team_number)
 @client.command()
 async def robot(ctx, team_number):
     team_number = int(team_number)
     robot = tba.team_robots(team_number)
-    print(robot)
-    #loop through the list of robots and print out the name and year
+
     message = ""
     if len(robot) == 0:
+        print_message('error', 'tba.team_robots', f'No robots found for team {team_number}')
+
         await ctx.send(f'Team {team_number} does not have any robots')
+
     else:
         message += (f'Team {team_number} has the following robots:\n')
+
         for robot in robot:
             message += f"{robot['robot_name']} in {robot['year']}\n"
+
+        print_message('info', 'tba.team_robots', f'Successfully sent {team_number} robots to channel')
+
         await ctx.send(message)
 
-#get team's media using tba.team_media(team_number, year)
+
+# get team's media using tba.team_media(team_number, year)
 @client.command()
 async def media(ctx, team_number, year):
     team_number = int(team_number)
     year = int(year)
-    media = tba.team_media(team_number, year)
-    print(media)
+
+    medias = tba.team_media(team_number, year)
+
     message = ""
-    if len(media) == 0:
+    if len(medias) == 0:
+        print_message('error', 'tba.team_media', f'No media found for team {team_number} in year {year}')
+
         await ctx.send(f'Team {team_number} does not have any media from {year}')
+
     else:
-        message += (f'Team {team_number} has the following media from {year}:\n')
-        for media in media:
-            #print media as an embed with the title as the media type and the description as the media direct link
+        message += f'Team {team_number} has the following media from {year}:\n'
+
+        for media in medias:
             embed = discord.Embed(title=media['type'], description=media['direct_url'])
+
             await ctx.send(embed=embed)
-            #
-            # message += f"{media['type']} - {media['direct_url']}\n"
+
+        print_message('info', 'tba.team_media', f'Successfully sent {team_number} media from {year} to channel')
+
         await ctx.send(message)
 
 
+'''
+method to print the time of the message in black and any message to the console in color given the type of message, subject the message itself
+@param type: the type of message to print
+@param message: the message to print
+@param subject: the subject of the message
+the subject is always printed in pink
+the message is always printed in light red
+the type is printed in the color corresponding to the type
 
+ex: 2022-11-15 15:52:09 INFO     discord.client logging in using static token
+'''
+
+
+def print_message(type, subject, message):
+    type = type.upper()
+
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f'\033[30m{time}', end=' ')
+
+    if type.lower() == 'success':
+        print(f'\033[92m{type}', end=' ')
+
+    elif type.lower() == 'warning':
+        print(f'\033[93m{type}', end=' ')
+
+    elif type.lower() == 'error':
+        print(f'\033[91m{type}', end=' ')
+
+    elif type.lower() == 'info':
+        print(f'\033[94m{type}', end=' ')
+
+    else:
+        print(f'\033[30m{type}', end=' ')
+
+    print(f'\033[95m{subject}', end=' ')
+
+    print(f'\033[91m{message}')
 
 
 client.run(TOKEN)
