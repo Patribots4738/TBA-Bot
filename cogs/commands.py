2d45d66577a933c5ce21d176eaddb7f8495964b9
Jacob1010-h <lordgaming1010@gmail.com>
2022-11-18 15:26:07 -0800

    comments
    COMMENTS

--- cogs/commands.py
+++ cogs/commands.py
@@ -5,14 +5,23 @@ import tbapy
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Loading the .env file and getting the tokens from it.
 load_dotenv()
 TOKEN = os.getenv('/.env/DISCORD_TOKEN')
 TBA_TOKEN = os.getenv('/.env/TBA_TOKEN')
 
+# Creating a variable called tba that is equal to the TBA API.
 tba = tbapy.TBA(TBA_TOKEN)
 
 
 def print_message(type, subject, message):
+    """
+    It takes in a type, subject, and message, and prints them out in a nice format
+
+    :param type: The type of message you want to print
+    :param subject: The subject of the message
+    :param message: The message you want to print
+    """
     type = type.upper()
 
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -39,19 +48,26 @@ def print_message(type, subject, message):
     print(f'\033[91m{message}')
 
 
-
 class Commands(commands.Cog):
 
     def __init__(self, client):
+        """
+        It's a function that prints a message to the console when the cog is loaded
+
+        :param client: The client that the cog is being loaded for
+        """
         self.client = client
         print_message('success', 'client.cogs.commands', 'Loaded cogs.commands')
 
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-    #     print_message('success', 'client.cogs.commands', 'Loaded cogs.commands')
-
-    @commands.command(help='testing help command')
+    @commands.command()
     async def event(self, ctx, team_number, year):
+        """
+        This function takes in a team number and a year, and returns a list of events that the team competed in that year
+
+        :param ctx: The context of where the command was called
+        :param team_number: The team number to get events for
+        :param year: The year to get events for
+        """
         team_number = int(team_number)
         year = int(year)
 
@@ -76,6 +92,13 @@ class Commands(commands.Cog):
 
     @commands.command()
     async def awards(self, ctx, team_number, year):
+        """
+        This function takes in a team number and a year and returns a list of awards that team won in that year
+
+        :param ctx: The context of where the command was used
+        :param team_number: The team number to get awards for
+        :param year: The year to get awards for
+        """
         team_number = int(team_number)
         year = int(year)
 
@@ -100,6 +123,12 @@ class Commands(commands.Cog):
 
     @commands.command()
     async def robot(self, ctx, team_number):
+        """
+        This function takes in a team number and returns a list of robots that team has had in the past
+
+        :param ctx: The context of where the command was used
+        :param team_number: The team number of the team you want to get the robots of
+        """
         team_number = int(team_number)
         robot = tba.team_robots(team_number)
         print(robot)
@@ -115,6 +144,13 @@ class Commands(commands.Cog):
 
     @commands.command()
     async def media(self, ctx, team_number, year):
+        """
+        This function gets the media for a team in a given year and sends it to the channel
+
+        :param ctx: The context of where the command was used
+        :param team_number: The team number to get media for
+        :param year: The year to get the media for
+        """
         team_number = int(team_number)
         year = int(year)
         # get team's media
@@ -139,6 +175,14 @@ class Commands(commands.Cog):
 
     @commands.command()
     async def alliance(self, ctx, event_key):
+        """
+        This function takes in an event key and returns the alliances for that event
+
+        :param ctx: The context as a Context object. This is a class in discord.py that contains metadata about the command
+        invocation
+        :param event_key: The TBA event key with the format yyyy[EVENT_CODE], where yyyy is the year, and EVENT_CODE is the
+        event code of the event
+        """
         # if the event key throws an error (not a valid event key) then send an error message to the channel
         try:
             alliances = tba.event_alliances(event_key)
@@ -171,5 +215,10 @@ class Commands(commands.Cog):
 
 
 async def setup(client):
+    """
+    It adds the cog to the client
+
+    :param client: The client that the cog is being setup for
+    """
     print(client)
     await client.add_cog(Commands(client))
--- main.py
+++ main.py
@@ -6,54 +6,51 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Loading the .env file and getting the tokens from it.
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 TBA_TOKEN = os.getenv('TBA_TOKEN')
 
 tba = tbapy.TBA(TBA_TOKEN)
 
+# Setting the intents and prefixes for the bot.
 intents = discord.Intents.all()
-
 client = commands.Bot(command_prefix='!', intents=intents)
 
 
 @client.event
 async def on_ready():
+    """
+    A function that is called when the bot is ready to start.
+    """
     print_message('success', 'discord.client', 'Bot is connected to Discord')
     print_message('info', 'discord.client', 'Loading cogs.commands')
     await client.load_extension('cogs.commands')
 
 
-'''
-It deletes the command message and then deletes the amount of messages specified by the user
-
-@param ctx: The context of where the command was used
-@param amount: The amount of messages to delete, defaults to 5 (optional)
-'''
-
-
 @client.command()
 async def clear(ctx, amount=1):
+    """
+    It clears the amount of messages you want to clear.
+
+    :param ctx: The context of where the command was used
+    :param amount: The amount of messages to delete, defaults to 1 (optional)
+    """
     if amount == -00:
         await ctx.channel.purge()
     else:
         await ctx.message.delete()
         await ctx.channel.purge(limit=amount)
 
-'''
-method to print the time of the message in black and any message to the console in color given the type of message, subject the message itself
-@param type: the type of message to print
-@param message: the message to print
-@param subject: the subject of the message
-the subject is always printed in pink
-the message is always printed in light red
-the type is printed in the color corresponding to the type
-
-ex: 2022-11-15 15:52:09 INFO     discord.client logging in using static token
-'''
-
 
 def print_message(type, subject, message):
+    """
+    It prints a message.
+
+    :param type: The type of message to print. This can be one of the following:
+    :param subject: The subject of the message
+    :param message: The message to be printed
+    """
     type = type.upper()
 
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
